19638b738ac02bc212a20d00a0c2fc3a76e69cdf
Eldar Nugaev <reldan0@gmail.com>
2025-03-05 20:16:07 -0800

    Add simple web socket

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 	"shortener/shortener"
 	"shortener/storage"
 	"sync"
@@ -27,11 +30,11 @@ func startConsumer(ch chan string, wg *sync.WaitGroup, short shortener.Shortener
 	}
 }
 
-func main() {
+func RunShortener() error {
 	table, err := storage.NewRedisStorage("localhost:6379", "", 0)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err
 	}
 	short := shortener.Shortener{Table: table}
 	wg := &sync.WaitGroup{}
@@ -44,4 +47,75 @@ func main() {
 	wg.Add(1)
 	go startProducer(ch, wg)
 	wg.Wait()
+	return nil
+}
+
+func main() {
+	RunWebServer()
+}
+
+// Socket example
+
+func RunWebServer() {
+	// Set up WebSocket route
+	http.HandleFunc("/ws", handleWebSocket)
+
+	// Serve static files (HTML) from current directory
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	// Start server
+	fmt.Println("Server starting on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// WebSocket upgrader
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// Allow all origins for this example
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// WebSocket connection handler
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	// Upgrade HTTP connection to WebSocket
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer func(conn *websocket.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Println("Close error:", err)
+		}
+	}(conn)
+
+	fmt.Println("Client connected")
+
+	// Main loop to handle messages
+	for {
+		// Read message from client
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+
+		// Print received message
+		fmt.Printf("Received: %s\n", message)
+
+		// Echo message back to client
+		err = conn.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+
+	fmt.Println("Client disconnected")
 }
